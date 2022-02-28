87720be3cd225a76b66540946bdbc84d5f364750
Shiv <shivaraj.thapa2021@gmail.com>
2022-02-28 16:42:32 -0600

    File create, write, read, and delete practice

---
+++ src/day9/FileInOut.java
@@ -0,0 +1,56 @@
+package day9;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FileInOut {
+    public static void main(String[] args) {
+
+        /* code to create a file
+        File myFile = new File("file.txt");
+        try {
+            myFile.createNewFile();
+        } catch (IOException e) {
+            System.out.println("Unable to create this file");
+            e.printStackTrace();
+        }
+
+
+        // code to write a file
+        try {
+            FileWriter fileWriter = new FileWriter("file.txt");
+        fileWriter.write("This is our first file from java course.\n okay now bye");
+        fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // code to read file
+        File myFile = new File("file.txt");
+        try {
+            Scanner sc = new Scanner(myFile);
+            while(sc.hasNextLine()){
+                String line = sc.nextLine();
+                System.out.println(line);
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+
+            e.printStackTrace();
+        }
+
+         */
+        // code to delete file
+
+        File myFile = new File("file.txt");
+        if(myFile.delete()){
+            System.out.println("I have deleted: " + myFile.getName());
+        }
+        else{
+            System.out.println("Unable to delete file");
+        }
+    }
+}
