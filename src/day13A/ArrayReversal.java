83657407531da9807fcc225f3afa2b41951a3b52
Shiv <shivaraj.thapa2021@gmail.com>
2022-03-08 21:57:37 -0600

    Array reversal

--- src/day13A/ArrayReversal.java
+++ src/day13A/ArrayReversal.java
@@ -1,4 +1,21 @@
 package day13A;
 
 public class ArrayReversal {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 21, 3, 4, 5, 34, 67};
+        int l = arr.length;
+        int n = Math.floorDiv(l, 2);
+        int temp;
+
+        for(int i =0; i<n; i++){
+            temp = arr[i];
+            arr[i] = arr[l-1-i];
+            arr[l-1-i] = temp;
+        }
+        for (int element: arr){
+            System.out.print(element + " ");
+        }
+    }
+
 }
