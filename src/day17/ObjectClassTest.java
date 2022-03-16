8b11d1e733d03d6273cd15c205583cc971a16246
Shiv <shivaraj.thapa2021@gmail.com>
2022-03-16 17:48:29 -0500

    Object class and its methods(toSting, hashCode, .equals)

---
+++ src/day17/ObjectClassTest.java
@@ -0,0 +1,29 @@
+package day17;
+
+import java.util.logging.Logger;
+
+public class ObjectClassTest {
+    public static void main(String[] args) {
+        Student student = new Student("Rush", 8, 10);
+        Student student1 = new Student("Rush", 8, 10);
+        // although the content are same these two objects are different
+        //these two objects have two memory location
+
+        Logger.getGlobal().info(student.toString());
+        Logger.getGlobal().info(student1.toString());
+        // to calculate hashcode
+        Logger.getGlobal().info(String.valueOf(student.hashCode()));
+        Logger.getGlobal().info(String.valueOf(student1.hashCode()));
+//        Logger.getGlobal().info(String.valueOf(student));
+
+//        student.toString();
+//        student.equals();
+//        student.hashCode();
+
+//        boolean isEqual = student ==student1;
+        boolean isEqual = student.equals(student1);
+        Logger.getGlobal().info(String.valueOf(isEqual));
+
+
+    }
+}
