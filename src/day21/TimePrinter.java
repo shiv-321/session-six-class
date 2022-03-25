3fcdafb4d3e0ab79a8acb8cf8ad21626b8f466bf
Shiv <shivaraj.thapa2021@gmail.com>
2022-03-25 15:46:16 -0500

    Callback in interface

--- src/day21/TimePrinter.java
+++ src/day21/TimePrinter.java
@@ -1,13 +1,21 @@
 package day21;
 
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.time.Instant;
 import java.util.logging.Logger;
 
 public class TimePrinter implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
-        Logger.getGlobal().info("Action Performed called");
+        System.out.println("At the tone, the time is "
+                + Instant.ofEpochMilli(e.getWhen()));
+        Toolkit.getDefaultToolkit().beep();
 
     }
 }
+
+/**
+ * this is an example of call back
+ */
