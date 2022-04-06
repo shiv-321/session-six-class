ca0aa4002b77b0b9d3ac0130a2a2aab18a23536f
Shiv <shivaraj.thapa2021@gmail.com>
2022-04-06 18:01:12 -0500

    Thread synchronization

---
+++ src/day25Synchronization/SynchronizePractice1.java
@@ -0,0 +1,45 @@
+package day25Synchronization;
+
+class Example{
+    void display(){
+        Thread g = Thread.currentThread();
+        for (int i =0; i<=5; i++){
+            try{
+                Thread.sleep(1000);
+                System.out.println(g.getName() + " " + i);
+            }catch (Exception e) {
+            }
+        }
+        synchronized (this){
+        for (int i =0; i<=5; i++){
+            try{
+                Thread.sleep(1000);
+                System.out.println(g.getName() + " " + i);
+            }catch (Exception e) {
+            }
+            }
+        }
+    }
+}
+class T extends Thread{
+    Example e;
+    T(Example e){
+        this.e =e;
+    }
+    public void run(){
+        e.display();
+    }
+}
+
+
+public class SynchronizePractice {
+    public static void main(String[] args) {
+        Example ex = new Example();
+        T t1 = new T(ex);
+        T t2 = new T(ex);
+        T t3 = new T(ex);
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
---
+++ src/day25Synchronization/SynchronizePractice3.java
@@ -0,0 +1,4 @@
+package day25Synchronization;
+
+public class SynchronizePractice3 {
+}
