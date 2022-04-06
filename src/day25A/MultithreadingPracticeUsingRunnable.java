35cfebffbe911c8498d2e1d1c132a33e04498e97
Shiv <shivaraj.thapa2021@gmail.com>
2022-04-06 00:55:02 -0500

    Thread priority practice

--- src/day25A/MultithreadingPracticeUsingRunnable.java
+++ src/day25A/MultithreadingPracticeUsingRunnable.java
@@ -15,20 +15,27 @@ class MyThreadRunnable2 implements Runnable {
     @Override
     public void run() {
         for (int i = 0; i < 1000; i++) {
-            System.out.println("I am a thread:2 not a threat");
+            System.out.println("I am a thread:2 not a threat: " + Thread.currentThread().getName());
         }
     }
 }
-public class MultithreadingPracticeUsingRunnable {
+public class MultithreadingPracticeUsingRunnable{
     public static void main(String[] args) {
         MyThreadRunnable1 t1 = new MyThreadRunnable1();
-        Thread thread1 = new Thread(t1);
+        Thread thread1 = new Thread(t1, "Shiv");
 
         MyThreadRunnable2 t2 = new MyThreadRunnable2();
-        Thread thread2 = new Thread(t2);
+        Thread thread2 = new Thread(t2, "Rhiv");
+
+        Thread thread3 = new Thread(new MyThreadRunnable1(), "Civ");
+
+        thread1.setPriority(Thread.MAX_PRIORITY);
+        thread2.setPriority(Thread.MIN_PRIORITY);
+        thread3.setPriority(Thread.NORM_PRIORITY);
 
         thread1.start();
         thread2.start();
+        thread3.start();
 
         // when we are implementing runnable interface we cannot apply the start method directly
 
--- src/day25A/ThreadConstructor.java
+++ src/day25A/ThreadConstructor.java
@@ -1,25 +1,30 @@
 package day25A;
 
-class MyThr extends Thread{
-    public MyThr(String name){
+class MyThr extends Thread {
+    public MyThr(String name) {
         super(name);
     }
-    public void run(){
-        System.out.println("Thank you");
+
+    public void run() {
+        for (int i = 0; i < 4000; i++) {
+            System.out.println("Thank you! " + this.getName());
 //        while (true){
 //            System.out.println("I am a thread");
 //        }
+        }
     }
 }
 public class ThreadConstructor {
     public static void main(String[] args) {
         MyThr t = new MyThr("Shiv");
         MyThr t2 = new MyThr("Aman");
+        t.setPriority(Thread.MAX_PRIORITY);
+        t2.setPriority(Thread.MIN_PRIORITY);
         t.start();
         t2.start();
-        System.out.println("The id of the thread: "+ t.getId());
-        System.out.println("The id of the thread: "+ t2.getId());
-        System.out.println("The name of the thread: "+ t.getName());
-        System.out.println("The name of the thread: "+ t2.getName());
-    }
+//        System.out.println("The id of the thread: "+ t.getId());
+//        System.out.println("The id of the thread: "+ t2.getId());
+//        System.out.println("The name of the thread: "+ t.getName());
+//        System.out.println("The name of the thread: "+ t2.getName());
+        }
 }
